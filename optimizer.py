2ad7df81f2159082331f5e6a2e6f431a5191097d
Yiruzz <arturofks@gmail.com>
2025-05-05 01:00:31 -0400

    feat: changed utility function to L2 norm

--- optimizer.py
+++ optimizer.py
@@ -16,21 +16,14 @@ class OptimizationModel:
         '''
         self.model = gp.Model(f'NonNegativeRealEstimation. NodeID: {id_node}')
         self.model.setParam('OutputFlag', 0)  # Suppress Gurobi output
+
         n = len(contingency_vector)
         
         # Decision variables
         x = self.model.addMVar(shape=n, lb=0.0, name="x")
 
-        # NOTE: The auxiliary variables are not necessary if we want to use another metric for distance. (Minimum squares)
-        # Auxiliary variables for absolute differences
-        abs_diff = self.model.addMVar(shape=n, lb=0.0, name="abs_diff")
-
-        # Add constraints to define abs_diff as |x[i] - contingency_vector[i]|
-        self.model.addConstrs((abs_diff[i] >= x[i] - contingency_vector[i] for i in range(n)), name="AbsDiffPos")
-        self.model.addConstrs((abs_diff[i] >= contingency_vector[i] - x[i] for i in range(n)), name="AbsDiffNeg")
-
-        # Objective function: minimize the sum of absolute differences
-        self.model.setObjective(gp.quicksum(abs_diff), gp.GRB.MINIMIZE)
+        # Objective function: minimize the sum of squared differences (L2 norm squared)
+        self.model.setObjective(gp.quicksum((x[i] - contingency_vector[i]) * (x[i] - contingency_vector[i]) for i in range(n)), gp.GRB.MINIMIZE)
 
         # Additional constraints
         for i, constraint in enumerate(constraints):
@@ -55,6 +48,7 @@ class OptimizationModel:
         '''
         self.model = gp.Model(f'RoundingEstimation. NodeID: {id_node}')
         self.model.setParam('OutputFlag', 0)
+        
         n = len(x_tilde)
         
         # Rounding problem
@@ -63,14 +57,8 @@ class OptimizationModel:
         
         y = self.model.addMVar(shape=n, vtype=gp.GRB.BINARY, name="y")
 
-        # Auxiliary variables for absolute differences
-        abs_diff = self.model.addMVar(shape=n, lb=0.0, name="abs_diff")
-
-        # Restriction: abs_diff = |residual_round - y|
-        self.model.addConstr(abs_diff >= residual_round - y)
-        self.model.addConstr(abs_diff >= y - residual_round)
-
-        self.model.setObjective(gp.quicksum(abs_diff), gp.GRB.MINIMIZE)
+        # Objective function: minimize the sum of squared differences (L2 norm squared)
+        self.model.setObjective(gp.quicksum((residual_round[i] - y[i]) * (residual_round[i] - y[i]) for i in range(n)), gp.GRB.MINIMIZE)
 
         x_rounded = x_floor + y
 
